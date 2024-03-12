17d47957a224a00a28e21b13b7c4b00a80833968
Murilo Alves <murilohenrique.ti@outlook.com.br>
2024-03-12 13:26:11 -0300

    first commit

---
+++ src/main/java/com/muriloalvesdev/interactivecalculator/InteractiveCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.muriloalvesdev.interactivecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muriloalvesdev/interactivecalculator/InteractiveCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muriloalvesdev.interactivecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractiveCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
