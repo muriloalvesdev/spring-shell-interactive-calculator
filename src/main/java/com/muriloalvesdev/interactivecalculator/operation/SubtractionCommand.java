3ec1628a9c827734ab4bb1b13c2733a605dd63b1
Murilo Alves <murilohenrique.ti@outlook.com.br>
2024-03-12 14:33:11 -0300

    impl subtract command

---
+++ src/main/java/com/muriloalvesdev/interactivecalculator/operation/SubtractionCommand.java
@@ -0,0 +1,41 @@
+package com.muriloalvesdev.interactivecalculator.operation;
+
+import com.muriloalvesdev.interactivecalculator.utils.OperationUtils;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+/**
+ * Componente do Spring Shell para realizar operações de subtração.
+ * Esta classe permite a subtração de números em uma expressão matemática fornecida pelo usuário.
+ * Nota: Esta classe pode realizar subtração de números em qualquer formato, exceto quando números negativos estão no início.
+ * O Spring Shell pode interpretar o hífen no início como uma opção não reconhecida, o que causa problemas de parsing.
+ * Exemplos aceitáveis de expressões:
+ * - "1 - 2- 3"
+ * - "1 - 2 - 3"
+ * - "1      -   2         -       3"
+ *
+ */
+@ShellComponent
+public class SubtractionCommand {
+
+    /**
+     * Método do Spring Shell para subtrair números em uma expressão matemática.
+     * @param expression A expressão matemática a ser processada. Esta expressão pode conter números e operadores de subtração.
+     */
+    @ShellMethod(value = "Subtract numbers", key = "subtract")
+    public void subtractNumbers(@ShellOption(help = "subtraction mathematical expression") String expression) {
+        String cleanExpression = OperationUtils.removeAllWhitespace(expression);
+
+        String[] numbers = OperationUtils.splitExpressionByOperator(cleanExpression, "-");
+
+        int result = Integer.parseInt(OperationUtils.removeCommasAndDots(numbers[0]));
+
+        for (int i = 1; i < numbers.length; i++) {
+            result -= Integer.parseInt(OperationUtils.removeCommasAndDots(numbers[i]));
+        }
+
+        System.out.println("result: " + result);
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/muriloalvesdev/interactivecalculator/utils/OperationUtils.java
@@ -0,0 +1,39 @@
+package com.muriloalvesdev.interactivecalculator.utils;
+
+/**
+ * Esta classe fornece métodos para manipulação de strings, como remoção de espaços em branco,
+ * divisão de strings por operadores e remoção de vírgulas e pontos.
+ */
+public final class OperationUtils {
+    private OperationUtils() {
+    }
+
+    /**
+     * Remove todos os espaços em branco de uma string.
+     * @param str A string da qual os espaços em branco serão removidos.
+     * @return A string resultante após a remoção dos espaços em branco.
+     */
+    public static String removeAllWhitespace(String str) {
+        return str.replaceAll(" ", "");
+    }
+
+    /**
+     * Divide uma string em substrings usando um operador específico como delimitador.
+     * @param str A string a ser dividida.
+     * @param operator O operador usado como delimitador para dividir a string (-, +, /, *).
+     * @return Um array de strings contendo as substrings resultantes da divisão.
+     */
+    public static String[] splitExpressionByOperator(String str, String operator) {
+        return str.split(operator);
+    }
+
+    /**
+     * Remove todas as vírgulas de uma string e substitui pontos por nada.
+     * @param str A string a ser processada.
+     * @return A string resultante após a limpeza das vírgulas e pontos.
+     */
+    public static String removeCommasAndDots(String str) {
+        return str.replaceAll(",", "").replaceAll("\\.", "");
+    }
+
+}
